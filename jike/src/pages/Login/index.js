d62a4123fd13cfbb34ece98f4bf071dffc0727e1
Luna <854280871@qq.com>
2024-10-31 17:11:08 +0800

    form valid

--- jike/src/pages/Login/index.js
+++ jike/src/pages/Login/index.js
@@ -3,17 +3,29 @@ import { Card, Form, Input, Button } from 'antd'
 import logo from '@/assets/logo.png'
 
 const Login = () => {
+  const onFinish=(value)=>{
+    console.log(value)
+  }
   return (
     <div className="login">
       <Card className="login-container">
         <img className="login-logo" src={logo} alt="" />
         {/* 登录表单 */}
-        <Form>
-          <Form.Item>
+        <Form onFinish={onFinish}>
+          <Form.Item
+            name="mobile"
+            rules={[
+              {required:true,message:'请输入手机号'},
+              {pattern:/^1[3-9]\d{9}$/,message:'请输入正确的手机号'}
+            ]}
+          >
             <Input size="large" placeholder="请输入手机号" />
           </Form.Item>
-          <Form.Item>
-            <Input size="large" placeholder="请输入验证码" />
+          <Form.Item
+            name="code"
+            rules={[{required:true,message:'请输入验证码'}]}
+          >
+            <Input size="large" placeholder="请输入验证码" maxLength={6}/>
           </Form.Item>
           <Form.Item>
             <Button type="primary" htmlType="submit" size="large" block>
