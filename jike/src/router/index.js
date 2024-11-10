0bfdba30b94ebf2370d493701f02ac1554acd67e
Luna <854280871@qq.com>
2024-11-10 16:49:39 +0800

    lazy loading

--- jike/src/router/index.js
+++ jike/src/router/index.js
@@ -2,9 +2,11 @@ import {createBrowserRouter} from 'react-router-dom'
 import Login from '@/pages/Login'
 import Layout from '@/pages/Layout'
 import AuthRoute from '@/components/AuthRoute'
-import Home from '@/pages/Home'
-import Article from '@/pages/Article'
-import Publish from '@/pages/Publish'
+import { lazy, Suspense } from 'react'
+
+const Publish=lazy(()=>import('@/pages/Publish'))
+const Article=lazy(()=>import('@/pages/Article'))
+const Home=lazy(()=>import('@/pages/Home'))
 
 const router=createBrowserRouter([
     {
@@ -17,15 +19,16 @@ const router=createBrowserRouter([
         children:[
             {
                 index:true,
-                element:<Home/>
+                element:<Suspense fallback={'加载中'}><Home/></Suspense>
+                
             },
             {
                 path:'article',
-                element:<Article/>
+                element:<Suspense fallback={'加载中'}><Article/></Suspense>
             },
             {
                 path:'publish',
-                element:<Publish/>
+                element:<Suspense fallback={'加载中'}><Publish/></Suspense>
             }
         ]
     }
